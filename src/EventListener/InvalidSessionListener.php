92ccbf7910149329e644940e91044d8227f7766f
Tomasz Kuter <evolic@interia.pl>
2022-06-29 16:01:28 +0200

    - Removed unused import from InvalidSessionListener

--- src/EventListener/InvalidSessionListener.php
+++ src/EventListener/InvalidSessionListener.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Loculus\SessionSecurityBundle\EventListener;
 
 use Loculus\SessionSecurityBundle\Event\InvalidSessionEvent;
-use Loculus\SessionSecurityBundle\InvalidationStrategyChain;
 use Loculus\SessionSecurityBundle\InvalidationStrategyManager;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
