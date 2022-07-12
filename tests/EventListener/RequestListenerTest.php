f0f267443408b61bf27e61ae03705d3d2c21f588
Tomasz Kuter <evolic@interia.pl>
2022-07-12 22:00:57 +0200

    SSB-003 fix(phpcs): added licence information to each PHP class file (#3)

--- tests/EventListener/RequestListenerTest.php
+++ tests/EventListener/RequestListenerTest.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * (c) Tomasz Kuter <tkuter@loculus.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Loculus\SessionSecurityBundle\Tests\EventListener;
